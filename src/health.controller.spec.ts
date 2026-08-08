1b00d59b658f861e0d11c85286c01590c8ffcb91
jaehyeon-25 <jh215420@gmail.com>
2026-08-08 14:18:40 +0900

    Revert "Merge branch 'work/backend'"
    This reverts commit 91d68a00713146f8dd7789d90ed2e57b58680b04, reversing
    changes made to e3dc74f36583e4c8bbfd56dc04bcdf4615f27e7d.

--- src/health.controller.spec.ts
+++
@@ -1,9 +0,0 @@
-import { HealthController } from './health.controller';
-
-describe('HealthController', () => {
-  it('returns ok status', () => {
-    const controller = new HealthController();
-
-    expect(controller.check()).toEqual({ status: 'ok' });
-  });
-});
